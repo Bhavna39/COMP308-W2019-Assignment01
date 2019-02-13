4f535d1cda0c91a6b44a9d7c4793b7b8379077fa
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-02-13 05:48:54 -0500

    updated package.json - engines

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Home" });
 });
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+router.get("/about", function(req, res, next) {
+  res.render("index", { title: "About" });
 });
-router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+router.get("/projects", function(req, res, next) {
+  res.render("index", { title: "Projects" });
 });
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+router.get("/services", function(req, res, next) {
+  res.render("index", { title: "Services" });
 });
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+router.get("/contact", function(req, res, next) {
+  res.render("index", { title: "Contact" });
 });
 
 module.exports = router;
