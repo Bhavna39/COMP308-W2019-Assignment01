92fd48952ebddc0ee696c61b9c4667f1a8efa573
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-02-13 04:40:34 -0500

    added ExpressJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
