0e7af9865fbabb4e1db0288b7ed518ad9bb63830
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-02-14 03:00:31 -0500

    Projects - Done

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,10 @@ router.get("/about", function(req, res, next) {
   res.render("about", { title: "About" });
 });
 router.get("/projects", function(req, res, next) {
-  res.render("index", { title: "Projects" });
+  res.render("projects", { title: "Projects" });
 });
 router.get("/services", function(req, res, next) {
-  res.render("index", { title: "Services" });
+  res.render("services", { title: "Services" });
 });
 router.get("/contact", function(req, res, next) {
   res.render("contact", { title: "Contact" });
