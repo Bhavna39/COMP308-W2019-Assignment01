e3bfce4b683b4db7fa30d39d2cfcaf8b9ea5a452
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-02-16 20:56:48 -0500

    Internal Documentation

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,14 +1,18 @@
-/* APP-Java Script file, Bhavna Pulliahgari - 300931671, 02/12/2019 */
+/**********************************/
+/*        Java Script File        */
+/*      Bhavna Pulliahgari        */
+/*          300931671             */
+/*       16th February, 2019      */
+/**********************************/
+
 "use strict";
 
 //IIFE
-(function()
-{
-    let mylocalVar = 0;
-    function Start()
-    {
-        console.log(`App started...${mylocalVar}`);
-    }
+(function() {
+  let mylocalVar = 0;
+  function Start() {
+    console.log(`App started...${mylocalVar}`);
+  }
 
-    window.addEventListener("load", Start);
-})();   
+  window.addEventListener("load", Start);
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
+/**********************************/
+/*       Java Script File         */
+/*      Bhavna Pulliahgari        */
+/*          300931671             */
+/*       16th February, 2019      */
+/**********************************/
+
+/* JS File for Routing */
 let express = require("express");
 let router = express.Router();
 
@@ -5,15 +13,23 @@ let router = express.Router();
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Home" });
 });
+
+/* GET about page. */
 router.get("/about", function(req, res, next) {
   res.render("about", { title: "About" });
 });
+
+/* GET projects page. */
 router.get("/projects", function(req, res, next) {
   res.render("projects", { title: "Projects" });
 });
+
+/* GET services page. */
 router.get("/services", function(req, res, next) {
   res.render("services", { title: "Services" });
 });
+
+/* GET contact page. */
 router.get("/contact", function(req, res, next) {
   res.render("contact", { title: "Contact" });
 });
